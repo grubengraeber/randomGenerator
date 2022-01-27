528873b9aae90a6f8da3866d92c0d1b3c86492dd
Fabio Tietz <fabiotietz0@gmail.com>
2022-01-28 00:48:54 +0100

    working for now

--- randomGenerator.py
+++ randomGenerator.py
@@ -1,5 +1,5 @@
-from random import randint
-from stat import filemode
+import random
+
 familie=["Frau", "Mama", "Vater", "Opa", "Oma", "Cousin", "Cousine", "Schwester", "Bruder", "Halbschwester", "Halbbruder"]
 nummer=["eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn"]
 sport=["laufen", "tanzen", "klettern", "tennis", "fußball", "kunstturnen", "skaten", "basketball"]
@@ -11,6 +11,64 @@ hobby=["löten", "schweißen", "drechseln", "sprachen", "reisen", "kino", "fortg
 eigenschaft=["freundlich", "grantig", "hilfsbereit", "müde", "energetisch", "gelangweilt", "empört", "erschrocken", "neugierig", "wütend"]
 gegenstand=["Hut", "Kopfhörer", "Handy", "Computer", "Stift", "Bett", "Auto", "Taschenlampe", "Ruderboot", "Papier"] 
 sprache=["deutsch", "englisch", "spanisch", "russisch", "französisch", "chinesisch", "japanisch", "yoruba", "farsi", "hebräisch"]
-while True:
-    print(familie[randint(0,len(familie)-1)]+" "+essen[randint(0,len(essen)-1)]+" "+film[randint(0,len(film)-1)])
-    break
+
+def familieFunc():
+        print (random.choice(familie))
+
+def nummerFunc():
+        print (random.choice(nummer))
+
+def sportFunc():
+        print (random.choice(sport))
+
+def farbeFunc():
+        print (random.choice(farbe))
+
+def songFunc():
+        print (random.choice(song))
+
+def essenFunc():
+        print (random.choice(essen))
+
+def filmFunc():
+        print (random.choice(film))
+
+def hobbyFunc():
+        print (random.choice(hobby))
+
+def eigenschaftFunc():
+        print (random.choice(eigenschaft))
+
+def gegenstandFunc():
+        print (random.choice(gegenstand))
+
+def spracheFunc():
+        print (random.choice(sprache))
+    
+#print("Gib für folgendes, folgendes ein: familie -> familieFunc(), nummer -> nummerFunc(), sport -> sportFunc(), farbe -> farbeFunc(), song -> songFunc(), essen -> essenFunc(), film -> filmFunc(), hobby -> hobbyFunc(), eigenschaft -> eigenschaftFunc(), gegenstand -> gegenstandFunc(), sprache -> spracheFunc();    Viel Spaß :)")
+# while True:
+print("sprache: ") 
+spracheFunc() 
+print("gegenstand: ") 
+gegenstandFunc() 
+print("eigenschaft: ") 
+eigenschaftFunc()
+print("hobby: ") 
+hobbyFunc()
+print("film: ") 
+filmFunc()
+print("essen: ") 
+essenFunc()
+print("song: ") 
+songFunc()
+print("farbe: ") 
+farbeFunc()
+print("sport: ") 
+sportFunc()
+print("nummer: ") 
+nummerFunc()
+print("familie: ") 
+familieFunc()
+#     print(familie[randint(0,len(familie)-1)]+" "+essen[randint(0,len(essen)-1)]+" "+film[randint(0,len(film)-1)]) 
+#     print(len(familie))
+#     break
