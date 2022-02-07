0438a9f765b351c2c2dc07e8abed005dd62fac62
Fabio Tietz <fabiotietz0@gmail.com>
2022-02-07 03:11:22 +0100

    Finished the version 1 of the GUI with full functionality, only thing is, that some list objects are repeating due to the random function

---
+++ GUI.py
@@ -0,0 +1,61 @@
+from cgitb import text
+import tkinter as tk
+from tkinter import CENTER, Variable, ttk
+from PIL import Image, ImageTk
+import random
+import lists as ls
+
+
+freunde = ["weibl", "männl", "sächl"]
+farben = ["blau", "grün", "weiß"]
+musik = ["pop", "rock", "edm"]
+liste_Auswahl = ["family", "numbers", "sports", "colors", "songs", "foods", "movies", "hobbies", "characteristics", "things", "languages"]
+auswahl_Dict = {"family": ls.family, "numbers": ls.numbers, "sports": ls.sports, "colors": ls.colors, "songs": ls.songs, "foods": ls.foods, "movies": ls.movies, "hobbies": ls.hobbies, "characteristics":ls.characteristics, "things": ls.things, "languages": ls.lanuages}
+
+def printEntryInput(selection):
+    return(chosenList.get())
+def howItWorks():
+   hierKoennte.set(random.choice(auswahl_Dict[chosenList.get()]))
+
+root = tk.Tk()
+root.title("Random Generator")
+root.geometry("800x400")
+root.minsize(width=250, height=250)
+root.maxsize(width=600, height=600)
+root.resizable(width=False, height=True)
+buttonText = tk.StringVar()
+buttonText.set("Push it")
+number = tk.IntVar()
+number.set(10)
+numberDecimal = tk.DoubleVar()
+numberDecimal.set(5.8)
+boolTime = tk.BooleanVar()
+boolTime.set(True)
+image = Image.open("ownProjects/python/randomGenerator/random.jpg").resize((300, 100))
+photo = ImageTk.PhotoImage(image)
+button1 = ttk.Button(root, textvariable=buttonText, padding="10", command=printEntryInput)
+quit_button = ttk.Button(root, text="End the session", command=root.destroy)
+entry1 = tk.Entry(root, justify=CENTER, width=30)
+hierKoennte = tk.StringVar()
+hierKoennte.set("Push the button and make it happen.")
+chosenList = tk.StringVar()
+chosenList.set(liste_Auswahl[0])
+
+opt = tk.OptionMenu(root, chosenList, *liste_Auswahl, command=printEntryInput)
+image = Image.open("ownProjects/python/randomGenerator/random.jpg").resize((300, 100))
+photo = ImageTk.PhotoImage(image)
+label4 = ttk.Label(root, image=photo)
+label1 = ttk.Label(root, text = "Welcome to randomGenerator!", font=("Arial", 20))
+label2 = ttk.Label(root, text="quick solutions for long morning attendance questions", font=("Arial", 12), padding=5)
+label3 = ttk.Label(root, textvariable=hierKoennte, padding=20)
+dummy_b = tk.Button(root, text='Try it out',command=howItWorks)
+
+label4.pack()
+label1.pack()
+label2.pack()
+opt.pack()
+dummy_b.pack()
+label3.pack() 
+quit_button.pack()
+
+root.mainloop()
---
+++ lists.py
@@ -0,0 +1,22 @@
+#familie
+family = ["Great-grandfather", "Great-grandmother","Great-uncle", "Grandfather", "Grandmother","Great-aunt", "Uncle", "Aunt", "Father", "Mother", "Sister", "Brother-in-law", "Brother", "Sister-in-law", "Husband", "Wife", "Cousin", "Cousin´s wife", "Cousin", "Cousin’s husband", "Nephew", "Niece", "Son", "Daughter-in-law", "Daughter", "Son-in-law", "First cousin once removed", "Grandson", "Granddaughter"]
+#nummer
+numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+#sport
+sports = ["laufen", "tanzen", "klettern", "tennis", "fußball", "kunstturnen", "skaten", "basketball"]
+#farbe
+colors = ["grün", "blau", "schwarz", "weiß", "gelb", "organge", "braun", "grau", "türkis", "cyan"]
+#song
+songs = ["purple noise  - Boris Brejcha", "wer hat die Kokosnuss geklaut - Sternschnuppe", "could you be loved - Bob Marley", "Under and over it - five finger death punch", "summer sun - Cari Cari", "TARAKA - Gordo", "I'm living - sizzla", "great plains - klangphonics", "Young black intelligent - Masta Ace", "Nikes - Frank Ocean"]
+#essen
+foods = ["Pizza", "Pasta", "Gulasch", "Auflauf", "Kaiserschmarrn", "Strudel", "Suppe", "Eierreis", "Salat", "Ofenkartoffel"] 
+#film
+movies = ["Batman Trilogie", "Marvel movies", "Fast and the furious", "Fluch der Karibik", "Harry Potter", "John Wick", "Kung Fu Panda", "James Bond", "Kingsman", "Interstellar"] 
+#hobby
+hobbies = ["löten", "schweißen", "drechseln", "sprachen", "reisen", "kino", "fortgehen", "essen gehen", "schießen gehen", "neues ausprobieren"] 
+#eigenschaft
+characteristics = ["freundlich", "grantig", "hilfsbereit", "müde", "energetisch", "gelangweilt", "empört", "erschrocken", "neugierig", "wütend"]
+#gegenstand
+things = ["Hut", "Kopfhörer", "Handy", "Computer", "Stift", "Bett", "Auto", "Taschenlampe", "Ruderboot", "Papier"] 
+#sprache
+lanuages = ["deutsch", "englisch", "spanisch", "russisch", "französisch", "chinesisch", "japanisch", "yoruba", "farsi", "hebräisch"]
